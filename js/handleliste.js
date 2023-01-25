89daa2c8a93f60ada4ee66d6cd2f1514b01aad44
millasteder <mida008@kristiania.student.no>
2023-01-25 09:23:52 +0100

    shoppinglist

---
+++ js/handleliste.js
@@ -0,0 +1,8 @@
+const bodyElement = document.querySelector('body');
+
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'Min handleliste';
+
+
+const pElement = document.createElement('p');
+pElement.textContent = 'lage din eigen';
